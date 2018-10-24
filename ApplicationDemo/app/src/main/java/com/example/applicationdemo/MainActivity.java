cb9cf800aad87a89dba31130e26987da3d56997b
liuyibin <liuyibin23@163.com>
2018-10-25 00:19:41 +0800

    主程序调用插件Activity,插件调用主程序Activity

--- ApplicationDemo/app/src/main/java/com/example/applicationdemo/MainActivity.java
+++ ApplicationDemo/app/src/main/java/com/example/applicationdemo/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.applicationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.qihoo360.replugin.RePlugin;
+import com.qihoo360.replugin.model.PluginInfo;
 import com.unnamed.b.atv.model.TreeNode;
 import com.unnamed.b.atv.view.AndroidTreeView;
 
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        initView();
 //        ViewGroup containerView = (ViewGroup) findViewById(R.id.container);
 //
 //        TreeNode root = TreeNode.root();
@@ -29,4 +35,25 @@ public class MainActivity extends AppCompatActivity {
 ////        mainLayout.addView(tView.getView());
 //        containerView.addView(tView.getView());
     }
+
+
+    private void initView(){
+        Button btnP1 = (Button) findViewById(R.id.btn_plugin1);
+        btnP1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PluginInfo info = RePlugin.getPluginInfo("plugindemo1");
+                info.isNeedUpdate();
+                if(info == null){
+                    Toast.makeText(MainActivity.this,"未找到plugindemo1",Toast.LENGTH_LONG).show();
+                } else {
+                    RePlugin.startActivity(MainActivity.this,RePlugin.createIntent("plugindemo1",
+                            "com.example.lyb.plugindemo1.MainActivity"));
+                }
+
+            }
+        });
+
+    }
+
 }
--- PluginDemo1/app/src/main/java/com/example/lyb/plugindemo1/MainActivity.kt
+++ PluginDemo1/app/src/main/java/com/example/lyb/plugindemo1/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.lyb.plugindemo1
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.qihoo360.replugin.RePlugin
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView(){
+        btn_host.setOnClickListener {
+            val hostContext = RePlugin.getHostContext()
+            if (hostContext == null){
+                Toast.makeText(this,"未找到host",Toast.LENGTH_LONG).show()
+            } else{
+//                RePlugin.startActivity(this@MainActivity,RePlugin.createIntent())
+                val intent = Intent()
+                intent.component = ComponentName("com.example.applicationdemo","com.example.applicationdemo.SecondActivity")
+                startActivity(intent)
+            }
+        }
     }
 }
