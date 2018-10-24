0e12fd0d01a6f9402c98fbb5224220edc566d393
liuyibin23 <liuyibin23@163.com>
2018-10-24 18:02:59 +0800

    主程序接入Replugin

---
+++ app/src/main/java/com/example/applicationdemo/MainApplication.java
@@ -0,0 +1,9 @@
+package com.example.applicationdemo;
+
+import com.qihoo360.replugin.RePluginApplication;
+
+/**
+ * Create By lyb on 2018/10/24
+ **/
+public class MainApplication extends RePluginApplication {
+}
