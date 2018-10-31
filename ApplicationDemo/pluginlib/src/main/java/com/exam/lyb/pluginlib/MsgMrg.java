f891b96af85788259369ccd553bfa8b51cc3cb67
liuyibin <liuyibin23@163.com>
2018-10-31 10:53:48 +0800

    修改aidl接口实现

--- ApplicationDemo/pluginlib/src/main/java/com/exam/lyb/pluginlib/MsgMrg.java
+++ ApplicationDemo/pluginlib/src/main/java/com/exam/lyb/pluginlib/MsgMrg.java
@@ -2,18 +2,15 @@ package com.exam.lyb.pluginlib;
 
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.util.Log;
 
 /**
  * Create By lyb on 2018/10/29
  **/
-public class MsgMrg implements IMsgInterface {
+public class MsgMrg extends IMsgInterface.Stub {
     @Override
     public void onMsgReceive(String msg) throws RemoteException {
-
+        Log.d("MsgMrg",msg);
     }
 
-    @Override
-    public IBinder asBinder() {
-        return null;
-    }
 }
